b1979c0484014fe6a12f9d56deb7ac95cf90eb2c
이동호 <edh1021@gmail.com>
2022-02-19 00:31:33 +0900

    init project

---
+++ Hyades-Back/src/main/java/com/hyades/HyadesBackApplication.java
@@ -0,0 +1,13 @@
+package com.hyades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyadesBackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HyadesBackApplication.class, args);
+  }
+
+}
---
+++ Hyades-Back/src/test/java/com/hyades/HyadesBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyadesBackApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
